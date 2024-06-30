dcff285c682ce4a90d97379ddfafaea7240f538f
ParkMinUk <parkmxxuk@gmail.com>
2024-07-01 02:45:30 +0900

    create line chart by building_id

--- svr.js
+++ svr.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/public', static(path.join(__dirname, 'public')));
 
+// /chartdatafromdb
 app.post('/chartdatafromdb', (req, res) => {
   console.log('chartdatafromdb 호출됨');
 
@@ -65,6 +66,56 @@ app.post('/chartdatafromdb', (req, res) => {
   });
 });
 
+// /chartdatafromdbwithbid
+app.post('/chartdatafromdbwithbid', (req, res) => {
+  console.log('chartdatafromdbwithbid 호출됨');
+
+  const bid = req.body.bid;
+  console.log('bid is %s', bid);
+
+  pool.getConnection((err, conn) => {
+    const resData = {};
+    resData.result = 'error';
+    resData.temp = [];
+    resData.reg_date = [];
+
+    if (err) {
+      conn.release(); // 반드시 해제
+      console.log('데이터베이스 연결 객체를 얻지 못함.');
+      res.json(resData);
+      return;
+    }
+
+    // DB에 data를 요청한다.
+    const exec = conn.query(
+      'select `temperature`, `reg_date` from `building_temperature` where `building_id` = ? order by reg_date asc',
+      [bid],
+      (err, rows) => {
+        if (err) {
+          conn.release(); // 반드시 해제
+          console.log('데이터 조회 실패.');
+          res.json(resData);
+          return;
+        }
+
+        // 조회가 된다면,
+        if (rows.length > 0) {
+          resData.result = 'ok';
+          rows.forEach((element) => {
+            resData.temp.push(element.temperature);
+            resData.reg_date.push(element.reg_date);
+          });
+        }
+        // query는 성공했지만, 조회된 데이터가 없다면,
+        else {
+          resData.result = 'none';
+        }
+        console.log('resData: ', resData);
+        return res.json(resData);
+      }
+    );
+  });
+});
 app.listen(3000, () => {
   console.log('Server Start');
 });
